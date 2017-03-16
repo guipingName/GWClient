5923729cd865a4b31790ed26d0edb3e2f05d8215
guipingName <guipingme@sina.com>
2017-03-16 17:03:09 +0800

    添加客户端项目

---
+++ GWClient/Application/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GWClient
+//
+//  Created by guiping on 17/3/16.
+//  Copyright © 2017年 guiping. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GWClient/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  GWClient
+//
+//  Created by guiping on 17/3/16.
+//  Copyright © 2017年 guiping. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
